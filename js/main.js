86dae6847260f20b742ba0d9b8e93d0278a8b5c5
taniot <gaetano.frascolla@gmail.com>
2023-11-21 12:13:43 +0100

    feat: clone

--- js/main.js
+++ js/main.js
@@ -14,8 +14,8 @@ Creare l’array di oggetti con le informazioni fornite.
 MILESTONE 1:
 Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 
-MILESTONE 2:
-Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
 BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
@@ -40,7 +40,6 @@ const team = [
     photo: 'wayne-barnett-founder-ceo.jpg',
   },
   {
-    name: 'Angela Caroll',
     role: 'Chief Editor',
     photo: 'angela-caroll-chief-editor.jpg',
   },
@@ -82,4 +81,95 @@ for (let i = 0; i < arrayLength; i++) {
   if (!(i === arrayLength - 1)) {
     console.log(`---`);
   }
+}
+
+//milestone 2
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+/*
+<li class="member">
+  <div>image</div>
+  <h3>Nome</h3>
+  <p>role</p>
+</li>
+*/
+
+const teamElement = document.querySelector('.team');
+
+// //template literal
+
+// for (let i = 0; i < team.length; i++) {
+//   const person = team[i];
+
+//   // const li = `<li class="member">
+//   //     <div>Immagine del membro: ${person.photo}</div>
+//   //     <h3>Nome del membro: ${person.name}</h3>
+//   //     <p>Ruolo del membro: ${person.role}</p>
+//   //   </li>`;
+
+//   let stringElement = '<li class="member">';
+//   stringElement += `<div>Immagine del membro: ${person.photo}</div>`;
+
+//   if (person.name !== undefined) {
+//     stringElement += `<h3>Nome del membro: ${person.name}</h3>`;
+//   }
+
+//   stringElement += `<p>Ruolo del membro: ${person.role}</p>`;
+//   stringElement += `</li>`;
+
+//   teamElement.innerHTML += stringElement;
+// }
+
+// //createElement
+
+// for (let i = 0; i < team.length; i++) {
+//   const person = team[i];
+
+//   //li
+//   const liElement = document.createElement('li');
+//   liElement.classList.add('member');
+
+//   const divImageElement = document.createElement('div');
+//   divImageElement.append(person.photo);
+
+//   const h3NameElement = document.createElement('h3');
+
+//   if (person.name !== undefined) {
+//     h3NameElement.textContent = person.name;
+//   } else {
+//     h3NameElement.textContent = 'Nome non trovato';
+//   }
+
+//   const pRoleElement = document.createElement('p');
+//   pRoleElement.innerText = person.role;
+
+//   liElement.append(divImageElement);
+//   liElement.append(h3NameElement);
+//   liElement.append(pRoleElement);
+
+//   teamElement.append(liElement);
+// }
+
+/*
+clone element
+*/
+
+const templateTeam = document.getElementById('team-template');
+
+for (let i = 0; i < team.length; i++) {
+  const person = team[i];
+
+  const liElement = templateTeam.content.cloneNode(true);
+
+  liElement.querySelector('.member .photo img').src = `img/${person.photo}`;
+  liElement.querySelector('.member .photo img').alt = person.name;
+
+  if (person.name !== undefined) {
+    liElement.querySelector('.member .name').innerText = person.name;
+  }
+
+  liElement.querySelector('.member .role').innerText = person.role;
+
+  teamElement.append(liElement);
 }
